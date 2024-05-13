190f5e9bc732ea5457886bd73782ede3a2da09ae
nic0-dev <mlcagas@up.edu.ph>
2024-05-13 09:12:30 +0800

    Added Thumbnail

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Mark | UXD Submission",
   description: "Submission for Maya UXD Internship",
+  imageUrl: "https://images.unsplash.com/photo-1711566724211-91feb6009d4b?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 };
 
 export default function RootLayout({children,}: {
